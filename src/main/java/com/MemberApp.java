16a8aceaa2c6b6c4717635cba645aedee13574d4
Administrator <Administrator@NNH1J2ASSQP10BA>
2019-05-10 15:25:04 +0800

    springcloud消费提供者

--- src/main/java/com/MemberApp.java
+++ src/main/java/com/MemberApp.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer//该注解的作用是将此服务注册到eureka服务中心
 public class MemberApp {
-
+   //消费提供者
 	public static void main(String[] args) {
 		SpringApplication.run(MemberApp.class, args);
 	}
